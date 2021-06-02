65bb471f4aa868ed811ddf9d9bcfcc90360f59c2
wilmerpachon <31974105+wilmer-pachon@users.noreply.github.com>
2021-06-02 17:09:08 -0500

    Cambios

---
+++ js/excel.js
@@ -0,0 +1,11 @@
+
+function descargarExcel(){
+    var tmpElemento = document.createElement('a');
+    var data_type = 'data:application/vnd.ms-excel';
+    var tabla_div = document.getElementById('dato');
+    var tabla_html = tabla_div.outerHTML.replace(/ /g, '%20');
+    tmpElemento.href = data_type + ', ' + tabla_html;
+    tmpElemento.download = 'Nombre_De_Mi_Excel.xls';
+    tmpElemento.click();
+}
+descargarExcel();
