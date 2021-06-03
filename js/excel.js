a0ec4af1ea015d04ef476326ba0272cb262e1a36
John Fredy Pérez <jhonfe649@gmail.com>
2021-06-02 22:24:22 -0500

    last changes

--- js/excel.js
+++ js/excel.js
@@ -1,16 +1,3 @@
-
-<<<<<<< Updated upstream
-function descargarExcel(){
-    var tmpElemento = document.createElement('a');
-    var data_type = 'data:application/vnd.ms-excel';
-    var tabla_div = document.getElementById('dato');
-    var tabla_html = tabla_div.outerHTML.replace(/ /g, '%20');
-    tmpElemento.href = data_type + ', ' + tabla_html;
-    tmpElemento.download = 'Nombre_De_Mi_Excel.xls';
-    tmpElemento.click();
-}
-descargarExcel();
-=======
 // function descargarExcel(){
 //     var tmpElemento = document.createElement('a');
 //     var data_type = 'data:application/vnd.ms-excel';
@@ -21,4 +8,7 @@ descargarExcel();
 //     tmpElemento.click();
 // }
 // descargarExcel();
->>>>>>> Stashed changes
+
+var dato = document.getElementById('dato');
+console.log(dato.value);
+
--- js/responses.js
+++ js/responses.js
@@ -1,5 +1,4 @@
 function getBotResponse(input) {
-    //rock paper scissors
     if (input == "rock") {
         return "paper";
     } else if (input == "paper") {
@@ -8,11 +7,41 @@ function getBotResponse(input) {
         return "rock";
     }
 
-    // Simple responses
-    if (input == "Buenas tardes") {
-        return "Muy buenas tardes en que podemos ayudarte? Desea saber sobre nuestros precios?";
+    // -------------------
+    if (input == "Buenos dias") {
+        return "Muy Buenos dias en que podemos ayudarte? Desea saber acerca de nuestros precios?";
     } else if (input == "Si") {
         return "Claro que si lo invitamos a comunicarse con nosotros a este numero....!";
+    } else if (input == "no") {
+        return "Te gustaria saber sobre nuestras promociones?";
+    } else if (input == "si") {
+        return "Claro que si, en el area de Blog se encuentran nuestras ultimas promociones";
+    } else if (input == "no") {
+        return "Lo invitamos a que se comunique con nosotros por medio de este correo y con mucho gusto responderemos tus dudas";
+    } else if(input == "Muchas gracias" ) {
+        return "Con mucho gusto estamos para ayudarte";
+    } else if (input == "Buenas noches") {
+        return "Muy Buenas noches en que podemos ayudarte? Desea saber acerca de nuestros precios?";
+    } else if (input == "Si") {
+        return "Claro que si lo invitamos a comunicarse con nosotros a este numero....!";
+    } else if (input == "no") {
+        return "Te gustaria saber sobre nuestras promociones?";
+    } else if (input == "si") {
+        return "Claro que si, en el area de Blog se encuentran nuestras ultimas promociones";
+    } else if (input == "no") {
+        return "Lo invitamos a que se comunique con nosotros por medio de este correo y con mucho gusto responderemos tus dudas";
+    } else if(input == "Muchas gracias" ) {
+        return "Con mucho gusto estamos para ayudarte";
+    } else if (input == "Buenas tardes") {
+        return "Muy Buenas tardes en que podemos ayudarte? Desea saber acerca de nuestros precios?";
+    } else if (input == "Si") {
+        return "Claro que si lo invitamos a comunicarse con nosotros a este numero....!";
+    } else if (input == "no") {
+        return "Te gustaria saber sobre nuestras promociones?";
+    } else if (input == "si") {
+        return "Claro que si, en el area de Blog se encuentran nuestras ultimas promociones";
+    } else if (input == "no") {
+        return "Lo invitamos a que se comunique con nosotros por medio de este correo y con mucho gusto responderemos tus dudas";
     } else if(input == "Muchas gracias" ) {
         return "Con mucho gusto estamos para ayudarte";
     }
