87fb2b3aa9d96725d1eb95fbea1f501576a505aa
wilmerpachon <31974105+wilmer-pachon@users.noreply.github.com>
2021-06-09 15:13:15 -0500

    cambios

---
+++ js/color.js
@@ -0,0 +1,5 @@
+let lnkBlog = document.querySelector('#lnk_b');
+let lnkNos = document.querySelector('#lnk_n');
+
+lnkBlog.addEventListener('click', () => lnkBlog.style.backgroundColor = '##010202')
+lnkNos.addEventListener('click', () => lnkNos.style.backgroundColor = '##010202')
