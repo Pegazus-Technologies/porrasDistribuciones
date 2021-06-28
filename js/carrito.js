c68fa2673514c72b532ae0291511b35daea44a56
wilmerpachon <31974105+wilmer-pachon@users.noreply.github.com>
2021-06-28 11:50:30 -0500

    cambios

--- js/carrito.js
+++ js/carrito.js
@@ -1,13 +1,19 @@
-let products =[]; 
-let total = 0;
+// let products =[]; 
+// let total = 0;
 
-function add(product, price) {
-    console.log(product, price);
-    products.push(product);
-    total = total + price;
-    document.getElementById("checkout").innerHTML = `Paga $${total}`
-}
+// function add(product, price) {
+//     console.log(product, price);
+//     products.push(product);
+//     total = total + price;
+//     document.getElementById("checkout").innerHTML = `Paga $${total}`
+// }
 
-function pay() {
-    swal(products.join(", \n"),'Enviado al formulario de pago', 'success');
-}
+$(function(){
+
+    ('#modal').modal({
+        backdrop: 'static',
+        keyboard: true,
+        focus: false,
+        show: false
+    });
+});
