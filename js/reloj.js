5df2141dd7b5ffd416797a4c1250347dcaa6e252
wilmerpachon <31974105+wilmer-pachon@users.noreply.github.com>
2021-06-30 09:59:34 -0500

    cambio

--- js/reloj.js
+++ js/reloj.js
@@ -19,3 +19,22 @@ function autoTimeUpdate (){
     document.getElementById('seconds').innerHTML = seconds;
 }
 setInterval(autoTimeUpdate,1000)
+
+// ---------------------------------------
+let mostraHora=()=>{
+    let reloj=document.getElementById('reloj')
+
+    let fechaHora= new Date()
+    let hora=fechaHora.getHours()
+    let minutos=fechaHora.getMinutes()
+    let segundos=fechaHora.getSeconds()
+    let hr=(hora>12) ? hora-12 : hora
+    let am = (hora>12) ? 'PM' : 'AM'
+    if(hr<10){hr='0' + hr}
+    if(minutos<10){minutos='0' + minutos}
+    if(segundos<10){segundos='0' + segundos}
+
+
+    reloj.textContent=`${hr}:${minutos}:${segundos} ${am}`
+}
+setInterval(mostraHora,1000)
